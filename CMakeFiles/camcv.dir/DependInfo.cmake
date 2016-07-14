564318268a197cb577ec0e08abf19be1b5185639
Vasilis Liaskovitis <vliaskov@gmail.com>
2016-07-15 01:28:50 +0300

    first commit

---
+++ CMakeFiles/camcv.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/tests/RaspiCLI.c" "/home/pi/tests/CMakeFiles/camcv.dir/RaspiCLI.c.o"
+  "/home/pi/tests/RaspiCamControl.c" "/home/pi/tests/CMakeFiles/camcv.dir/RaspiCamControl.c.o"
+  "/home/pi/tests/RaspiPreview.c" "/home/pi/tests/CMakeFiles/camcv.dir/RaspiPreview.c.o"
+  "/home/pi/tests/RaspiTex.c" "/home/pi/tests/CMakeFiles/camcv.dir/RaspiTex.c.o"
+  "/home/pi/tests/RaspiTexUtil.c" "/home/pi/tests/CMakeFiles/camcv.dir/RaspiTexUtil.c.o"
+  "/home/pi/tests/camcv.c" "/home/pi/tests/CMakeFiles/camcv.dir/camcv.c.o"
+  "/home/pi/tests/gl_scenes/mirror.c" "/home/pi/tests/CMakeFiles/camcv.dir/gl_scenes/mirror.c.o"
+  "/home/pi/tests/gl_scenes/models.c" "/home/pi/tests/CMakeFiles/camcv.dir/gl_scenes/models.c.o"
+  "/home/pi/tests/gl_scenes/sobel.c" "/home/pi/tests/CMakeFiles/camcv.dir/gl_scenes/sobel.c.o"
+  "/home/pi/tests/gl_scenes/square.c" "/home/pi/tests/CMakeFiles/camcv.dir/gl_scenes/square.c.o"
+  "/home/pi/tests/gl_scenes/teapot.c" "/home/pi/tests/CMakeFiles/camcv.dir/gl_scenes/teapot.c.o"
+  "/home/pi/tests/gl_scenes/yuv.c" "/home/pi/tests/CMakeFiles/camcv.dir/gl_scenes/yuv.c.o"
+  "/home/pi/tests/tga.c" "/home/pi/tests/CMakeFiles/camcv.dir/tga.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/opt/vc/include/interface/vcos"
+  "/opt/vc/include"
+  "/opt/vc/include/interface/vcos/pthreads"
+  "/opt/vc/include/interface/vmcs_host/linux"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/displayimage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/displayimage.dir/display_image.cpp.o"
+  "displayimage.pdb"
+  "displayimage"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/displayimage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
